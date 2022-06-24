606f070d15fd70b554c3e7cfdbd925a43c09aef3
puritybirir <puritybirir@gmail.com>
2022-06-24 14:24:06 +0300

    create special characters and escape sequences

---
+++ Strings/specialchar.js
@@ -0,0 +1,8 @@
+// You want to insert a special character, such as a line break, into a string.
+
+// use escape sequences
+const favoriteMovie = 'My favorite movie is \'ShawShank Redemption\'.'
+console.log(favoriteMovie);
+
+const copyrightNotice = 'This page \u00A9 Shelley Powers.';
+console.log(copyrightNotice);
