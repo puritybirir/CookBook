491f4809464882b13299c0383f9231bca25106fb
puritybirir <puritybirir@gmail.com>
2022-06-28 21:50:43 +0300

    String literals to replace addition concatenation

---
+++ Strings/templateliterals.js
@@ -0,0 +1,14 @@
+// You want a simpler, clearer way to write long string concatenation operations.
+
+const greeting = `Hello world from a template literal`;
+
+const firstName = "Purity";
+const lastName = "Birir";
+const hireDate = "5th of June 2021";
+const team = "Maze"
+
+employeeDetail = `Our team includes ${firstName} ${lastName} who works on the
+${team} team. They've been a team member since ${hireDate}!`;
+
+console.log(employeeDetail);
+
